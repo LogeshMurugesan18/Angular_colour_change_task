4e47910cbb7d6e7fa5b1aa4ea112b31ab9e87a2c
LogeshMurugesan18 <lokeshmurugesan18@gmail.com>
2025-05-29 23:15:40 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'color_change_task';
+  title = 'Color Change Task';
+  inputcolor = "";
+  inputcolor2 = "";
+  btncolor = "cyan";
+  btncolor2 = "cyan";
+
+  colorchange(){
+  if(this.inputcolor && CSS.supports('color',this.inputcolor)){
+      this.btncolor=this.inputcolor;
+    }else{
+      this.btncolor="cyan";
+    }  }
+
+   autochange(){
+    if(this.inputcolor2 && CSS.supports('color',this.inputcolor2)){
+      this.btncolor2=this.inputcolor2;
+    }else{
+      this.btncolor2="cyan";
+    }
+  }
 }
